5438c8d6482892c415ba9c26646bd783463b953d
JasonYang <jason19901207@gmail.com>
2018-10-01 23:22:29 +0800

    新增day19特效

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -32,29 +32,73 @@ function paintToCanvas() {
         //做效果
         // pixels = redEffect(pixels);
         // pixels = redSplit(pixels);
-        pixels = greenScreen(pixels);
-        ctx.globalAlpha = 0.5;
+        // pixels = greenScreen(pixels);
+        horizontalFlip = horizontalFlip(pixels);
+        // pixels = negativeEffect(pixels);
+        // pixels = grayscaleEffect(pixels);
+        // ctx.globalAlpha = 0.05;
         //放回
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
+/* 泛紅 */
 function redEffect(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i + 0] += 80;
-        pixels.data[i + 1] -= 50;
-        pixels.data[i + 2] *= 0.5;
+        pixels.data[i + 0] += 100;//red
+        pixels.data[i + 1] -= 100;//green
+        pixels.data[i + 2] *= 0.5;//blue
     }
     return pixels;
 }
 
+
 function redSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i - 150] = pixels.data[i + 0];
-        pixels.data[i + 500] = pixels.data[i + 1];
-        pixels.data[i - 550] = pixels.data[i + 2];
+        pixels.data[i - 150] = pixels.data[i + 0]; //red
+        pixels.data[i + 500] = pixels.data[i + 1]; //green
+        pixels.data[i - 550] = pixels.data[i + 2]; //blue
     }
     return pixels;
 }
+
+/* 負片 */
+function negativeEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4){
+        pixels.data[i + 0] = 255 - pixels.data[i + 0]; //red
+        pixels.data[i + 1] = 255 - pixels.data[i + 1]; //green
+        pixels.data[i + 2] = 255 - pixels.data[i + 2]; //blue
+    }
+    return pixels;
+}
+/* 灰階 */
+function grayscaleEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        let avg = (pixels.data[i + 0] + pixels.data[i + 1] + pixels.data[i + 2]) / 3;
+        pixels.data[i + 0] = avg; //red
+        pixels.data[i + 1] = avg; //green
+        pixels.data[i + 2] = avg; //blue
+    }
+    return pixels;
+}
+// /* 鏡像效果 */
+// function mirror(pixels, width, height) {
+//     for (let i = 0; i < pixels.data.length; i += 4) {
+//         let newPoint = 
+//     }
+// }
+
+/* 水平翻轉效果 */
+function horizontalFlip(pixels) {
+    let newPixels = [];
+    for (let i = 0; i < pixels.data.length; i += 4){
+        let iMirror = (width - ((i / 4) % width)) * 4 + Math.floor(i / 4 / width) * 4 * width;
+        pixels.data[i + 0] = pixels.data[iMirror + 0]; // red
+        pixels.data[i + 1] = pixels.data[iMirror + 1]; // green
+        pixels.data[i + 2] = pixels.data[iMirror + 2]; // blue
+    }
+    return pixels;
+}
+
 function greenScreen(pixels) {
     const levels = {};
     document.querySelectorAll('.rgb input').forEach((input) => {
