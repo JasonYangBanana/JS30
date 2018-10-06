28cb17f3af256e0d69fb74703890a4086fcde553
JasonYang <jason19901207@gmail.com>
2018-10-06 10:47:24 +0800

    修改day16～20

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,12 +3,13 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+let effectIndex = 0;
+const effectType = ['normal', 'redEffect', 'oldStyle', 'redSplit', 'negativeEffect', 'grayScaleEffect', 'greenScreen']
+const rgb = document.querySelector('.rgb');
 
 function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(mediaStream => {
-            console.log(mediaStream);
-            // mediaStream = new MediaStream();
             try {
                 video.srcObject = mediaStream;
             } catch (err) {
@@ -24,18 +25,37 @@ function paintToCanvas() {
     const height = video.videoHeight;
     canvas.width = width;
     canvas.height = height;
-    console.log(width, height);
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
         //取出pixels
         let pixels = ctx.getImageData(0, 0, width, height);
+        rgb.style.display = 'none';
         //做效果
-        // pixels = redEffect(pixels);
-        // pixels = redSplit(pixels);
-        // pixels = greenScreen(pixels);
-        horizontalFlip = horizontalFlip(pixels);
-        // pixels = negativeEffect(pixels);
-        // pixels = grayscaleEffect(pixels);
+        switch (effectType[effectIndex]) {
+            case 'redEffect':
+                pixels = redEffect(pixels);
+                break;
+            case 'oldStyle':
+                pixels = oldStyle(pixels);
+                break;
+            case 'redSplit':
+                pixels = redSplit(pixels);
+                break;
+            case 'negativeEffect':
+                pixels = negativeEffect(pixels);
+                break;
+            case 'grayScaleEffect':
+                pixels = grayScaleEffect(pixels);
+                break;
+            case 'greenScreen':
+                rgb.style.display = 'block';
+                pixels = greenScreen(pixels);
+                break;
+            default:
+                pixels = ctx.getImageData(0, 0, width, height);
+                break;
+        }
+        // pixels = horizontalFlip(pixels, width, height);
         // ctx.globalAlpha = 0.05;
         //放回
         ctx.putImageData(pixels, 0, 0);
@@ -50,8 +70,18 @@ function redEffect(pixels) {
     }
     return pixels;
 }
+/* 復古 */
+function oldStyle(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] += 150; //red
+        pixels.data[i + 1] += 50; //green
+        pixels.data[i + 2] += 50; //blue
+        pixels.data[i + 3] *= 0.9; //alpha
+    }
+    return pixels;
+}
 
-
+/* 色相分裂 */
 function redSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
         pixels.data[i - 150] = pixels.data[i + 0]; //red
@@ -71,7 +101,7 @@ function negativeEffect(pixels) {
     return pixels;
 }
 /* 灰階 */
-function grayscaleEffect(pixels) {
+function grayScaleEffect(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
         let avg = (pixels.data[i + 0] + pixels.data[i + 1] + pixels.data[i + 2]) / 3;
         pixels.data[i + 0] = avg; //red
@@ -88,16 +118,21 @@ function grayscaleEffect(pixels) {
 // }
 
 /* 水平翻轉效果 */
-function horizontalFlip(pixels) {
-    let newPixels = [];
-    for (let i = 0; i < pixels.data.length; i += 4){
-        let iMirror = (width - ((i / 4) % width)) * 4 + Math.floor(i / 4 / width) * 4 * width;
-        pixels.data[i + 0] = pixels.data[iMirror + 0]; // red
-        pixels.data[i + 1] = pixels.data[iMirror + 1]; // green
-        pixels.data[i + 2] = pixels.data[iMirror + 2]; // blue
-    }
-    return pixels;
-}
+// function horizontalFlip(pixels, width, height) {
+//     let newPixels = [];
+//     for (let i = 0; i < pixels.data.length; i += 4){
+//         let iMirror = (width - ((i / 4) % width)) * 4 + Math.floor(i / 4 / width) * 4 * width;
+//         newPixels[i + 0] = pixels.data[iMirror + 0]; // red
+//         newPixels[i + 1] = pixels.data[iMirror + 1]; // green
+//         newPixels[i + 2] = pixels.data[iMirror + 2]; // blue
+//         newPixels[i + 3] = 255;
+
+//     }
+//     for (let j = 0; j < newPixels.length; j += 4){
+//         pixels.data[j] = newPixels[j];
+//     }
+//     return pixels;
+// }
 
 function greenScreen(pixels) {
     const levels = {};
@@ -133,6 +168,11 @@ function takePhoto() {
     link.setAttribute('download', 'handsome');
     link.innerHTML = `<img src="${data}" alt="Handsome Man">`
     strip.insertBefore(link, strip.firstChild);
+}
+
+function switchEffect() {
+    effectIndex += 1;
+    effectIndex %= effectType.length;
 }
 
 getVideo();
