3a70afeb698ccdfdc8864e90c9cff0ae035e7274
JasonYang <jason19901207@gmail.com>
2018-10-12 15:23:00 +0800

    完成day24

--- 03 - CSS Variables/js/index.js
+++ 03 - CSS Variables/js/index.js
@@ -1,7 +1,8 @@
 const inputs = document.querySelectorAll('.controls input');
 function handlerUpdate() {
+    console.log(this.value);
     const dataSizing = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + dataSizing);
 }
-inputs.forEach(value => value.addEventListener('change', handlerUpdate));
+// inputs.forEach(value => value.addEventListener('change', handlerUpdate));
 inputs.forEach(value => value.addEventListener('mousemove', handlerUpdate));
