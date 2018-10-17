275c6d5b017716addd76f2edae826b447cb5a0a6
JasonYang <jason19901207@gmail.com>
2018-10-17 18:49:51 +0800

    修正時鐘bug，一開始指針會旋轉的問題

--- 02 - JS and CSS Clock/js/clock.js
+++ 02 - JS and CSS Clock/js/clock.js
@@ -1,13 +1,12 @@
 //let hand = document.querySelector('.hand');
-let secHand = document.querySelectorAll('.second-hand');
-let minHand = document.querySelectorAll('.min-hand');
+let secHand = document.querySelector('.second-hand');
+let minHand = document.querySelector('.min-hand');
 let hrHand = document.querySelectorAll('.hour-hand');
 let now = new Date();
 let secs = now.getSeconds();
 let mins = now.getMinutes();
 let hrs = now.getHours();
 console.log(hrs);
-let round = 0;
 let secCount = 0
 
 let dayNight= document.querySelectorAll('.day-and-night');
@@ -21,21 +20,15 @@ londonHr - 12 < 0 ? dayNight[1].innerHTML = "A.M." : dayNight[1].innerHTML = "P.
 tokyoHr - 12 < 0 ? dayNight[2].innerHTML = "A.M." : dayNight[2].innerHTML = "P.M.";
 
 function setDate() {
-    if (secs === 0) {
-        round++;
-    }
+    
     //90deg是將指針從9時移動到12時
     //secCount是每一秒的位移量
-    let secsDegree = (secs + secCount + 60 * round) * 360 / 60 + 90;
+    let secsDegree = (secs + secCount) * 360 / 60 + 90;
     let minsDegree = (mins) * 360 / 60 + (secs + secCount) * 360 / 60 / 60 + 90;
     let hrsDegree = (hrs) * 360 / 12 + mins * 360 / 60 / 12 + (secs + secCount) * 360 / 60 / 60 / 12 + 90;
     secCount += 1;
-    secHand.forEach(function (value, index, listObj) {
-        value.style.transform = `rotate(${secsDegree}deg)`;
-    });
-    minHand.forEach(function (value, index, listObj) {
-        value.style.transform = `rotate(${minsDegree}deg)`;
-    });
+    secHand.style.transform = `rotate(${secsDegree}deg)`;
+    minHand.style.transform = `rotate(${minsDegree}deg)`;
     hrHand.forEach(function (value, index, listObj) {
         if (index === 0) {
             value.style.transform = `rotate(${hrsDegree}deg)`;
@@ -51,4 +44,5 @@ function setDate() {
         };
     });
 }
+setDate();
 setInterval(setDate, 1000);
