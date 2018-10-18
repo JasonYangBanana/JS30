5cf5912dd0d7b160df5691a6dec2c9e875ccc886
JasonYang <jason19901207@gmail.com>
2018-10-18 11:24:17 +0800

    復原day3註解掉的監聽'change'事件的

--- 03 - CSS Variables/js/index.js
+++ 03 - CSS Variables/js/index.js
@@ -1,8 +1,7 @@
 const inputs = document.querySelectorAll('.controls input');
 function handlerUpdate() {
-    console.log(this.value);
     const dataSizing = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + dataSizing);
 }
-// inputs.forEach(value => value.addEventListener('change', handlerUpdate));
+inputs.forEach(value => value.addEventListener('change', handlerUpdate));
 inputs.forEach(value => value.addEventListener('mousemove', handlerUpdate));
